13817776504adcffd5a336f8e6d31e6bfb9c0a63
kchung1068 <kchung1068@stu.d214.org>
2021-02-03 14:39:10 -0600

    commit 1

---
+++ src/APLine.java
@@ -0,0 +1,25 @@
+public class APLine {
+	private int a;
+	private int b;
+	private int c;
+
+	private double slope;
+
+	public APLine(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double getSlope() {
+		return (double)(a) * -1 / (double)(b);
+	}
+
+	public boolean isOnLine(int x, int y) {
+		if ((a * x) + (b * y) + (c) == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ src/APLineTester.java
@@ -0,0 +1,17 @@
+public class APLineTester {
+    public static void main(String[] args) {
+        APLine line1 = new APLine(5, 4, -17);
+
+        double slope1 = line1.getSlope();                 // slope1 is assigned -1.25
+
+        boolean onLine1 = line1.isOnLine(5, -2);     // true because 5(5) + 4(-2) + (-17) = 0
+
+
+
+        APLine line2 = new APLine(-25, 40, 30);
+
+        double slope2 = line2.getSlope();                 // slope2 is assigned 0.625
+
+        boolean onLine2 = line2.isOnLine(5, -2);      // false because -25(5) + 40(-2) + 30 ≠ 0
+    }
+}
